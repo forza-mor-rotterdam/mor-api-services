78fb9b61397ff91599af9cb7870d3c36c4449528
Maurice Guikema <maurice@mgui.nl>
2026-01-14 16:18:06 +0100

    melding-afgesloten to POST

--- mor_api_services/signaalapplicatie.py
+++ mor_api_services/signaalapplicatie.py
@@ -6,6 +6,6 @@ logger = logging.getLogger(__name__)
 class SignaalapplicatieService(BasisService):
     _gebruik_token = True
 
-    def notificatie_melding_afgesloten(self, signaal_uri):
+    def notificatie_melding_afgesloten(self, signaal_uri, data={}):
         url = f"{signaal_uri}melding-afgesloten/"
-        return self.do_request(url, method="post", data={}, cache_timeout=0)
+        return self.do_request(url, method="post", data=data, cache_timeout=0)
